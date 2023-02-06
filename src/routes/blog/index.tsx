ab81226d5c9c4636631263b528adc3a61e3e42fc
Alessandro Sebastiani <sebbalex@gmail.com>
2023-02-07 00:04:29 +0100

    feat: blog styling

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 import posts from "./post.json";
 
@@ -7,13 +7,27 @@ export default component$(() => {
     id: number;
     title: string;
     description: string;
+    specification: string;
     author: string;
     slug: string;
     image: string;
     avatar: string;
     cta: string;
+    motto: string;
     created_at: string;
+    pros: string[];
+    cons: string[];
+    review: string;
+    stars: number;
+    tags: string[];
   }
+  const stars = $((s: number) => {
+    let ss = "";
+    for (let i = 0; i < s; i++) {
+      ss += "⭐️";
+    }
+    return ss;
+  });
 
   const blogs: Blogs[] = posts;
   return (
@@ -21,23 +35,43 @@ export default component$(() => {
       <h1 class="text-3xl">Posts</h1>
       {blogs.map((blog) => (
         <>
-          <div class="card lg:card-side bg-base-100 shadow-xl">
+          <div class="card w-4/5 bg-base-100 shadow-xl">
             <figure>
               <Link key={blog.id} href={blog.slug}>
-                <img
-                  class="object-cover h-full rounded-t-lg w-96 md:w-96 md:h-full md:rounded-none md:rounded-l-lg"
-                  src={blog.image}
-                  alt={blog.title}
-                />
+                <img src={blog.image} alt={blog.title} />
               </Link>
             </figure>
             <div class="card-body">
               <Link key={blog.id} href={blog.slug}>
-                <h2 class="card-title">
+                <h2 class="card-title pb-7">
                   {blog.title}
                   <div class="badge badge-secondary">NEW</div>
                 </h2>
                 <p>{blog.description}</p>
+                <hr class="pt-5" />
+                <h2 class="card-title">Specificaition</h2>
+                <p>{blog.specification}</p>
+                <hr class="pt-7" />
+                <h2 class="card-title justify-center">{blog.title}</h2>
+                <h3 class="flex font-medium justify-center">{blog.motto}</h3>
+                <hr class="pt-7" />
+                <h2 class="card-title">Reasons</h2>
+                <h3 class="pt-2 font-medium">To buy</h3>
+                <ul>
+                  {blog.pros.map((pro) => (
+                    <li>✅ {pro}</li>
+                  ))}
+                </ul>
+                <h3 class="pt-2 font-medium">To avoid</h3>
+                <ul>
+                  {blog.cons.map((con) => (
+                    <li>⭕️ {con}</li>
+                  ))}
+                </ul>
+                <hr class="pt-5" />
+                <h2 class="card-title">Review</h2>
+                {stars(blog.stars)}
+                <p>{blog.review}</p>
               </Link>
               <div class="flex items-center pt-5">
                 <img
@@ -51,48 +85,21 @@ export default component$(() => {
                 </div>
               </div>
               <div class="card-actions justify-end">
+                {blog.tags.map((t) => (
+                  <div class="badge badge-outline">{t}</div>
+                ))}
+              </div>
+              <div class="card-actions justify-end">
+                <img
+                  class="w-40 h-15 rounded-full mr-4"
+                  src="/assets/images/amazon.png"
+                />
                 <a href={blog.cta} class="btn btn-primary">
                   Buy
                 </a>
               </div>
             </div>
           </div>
-
-          {/* <div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-2xl hover:bg-gray-100">
-            <Link key={blog.id} href={blog.slug}>
-              <img
-                class="object-cover w-full rounded-t-lg h-96 md:h-56 md:w-full md:rounded-none md:rounded-l-lg"
-                src={blog.image}
-                alt={blog.title}
-              />
-            </Link>
-            <div class="flex flex-col justify-between p-4 leading-normal">
-              <Link key={blog.id} href={blog.slug}>
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {blog.title}
-                </h5>
-              </Link>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {blog.description}
-              </p>
-              <div class="flex items-center">
-                <img
-                  class="w-10 h-10 rounded-full mr-4"
-                  src={blog.avatar}
-                  alt={`avatar of ${blog.title}`}
-                />
-                <div class="text-sm">
-                  <p class="text-gray-900 leading-none">{blog.author}</p>
-                  <p class="text-gray-600">{blog.created_at}</p>
-                </div>
-              </div>
-              <div class="flex items-center pt-3 justify-end">
-                <a href={blog.cta} class="btn">
-                  Buy
-                </a>
-              </div>
-            </div>
-          </div> */}
         </>
       ))}
     </div>
